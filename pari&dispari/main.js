a22505cdb13c7f373131102c6e721b49faa53842
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-10-28 15:41:08 +0200

    Aggiunto esercizio 2 pari o dispari

--- pari&dispari/main.js
+++ pari&dispari/main.js
@@ -10,10 +10,39 @@
                                     FUNCTIONS
 ----------------------------------------------------------------------------*/
 
+function whoHasWin(num1, num2){
 
+    let sum = num1 + num2;
+
+    if(sum % 2 === 0 && userChoice === "Pari"){
+
+        alert("Hai vinto!");
+
+    }else if(sum % 2 === 1 && userChoice === "Pari"){
+
+        alert("Hai perso!");
+
+    }else if(sum % 2 === 1 && userChoice === "Dispari"){
+
+        alert("Hai vinto!");
+
+    }else if(sum % 2 === 0 && userChoice === "Dispari"){
+
+        alert("Hai perso!");
+
+    }
+}
 
 /*---------------------------------------------------------------------------
                                     MAIN
 ----------------------------------------------------------------------------*/
 
+// Chiedo la scelta e un numero all'utente
 const userChoice = prompt("Scegli pari o dispari?");
+const userNumber = Number(prompt("Ora scegli un numero da 1 a 5!"));
+
+// Assegno un numero random alla cpu
+let cpuNumber = Math.floor(Math.random() * 5) + 1;
+
+// Uso la funzione per decretare il vincitore
+whoHasWin(userNumber, cpuNumber);
